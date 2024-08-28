2e0da1fe59b0cc6321db5401f30ab8b090a670d8
PiterAroni <118916058+PiterAroni@users.noreply.github.com>
2024-08-28 17:41:03 -0500

    Update bd.php

--- admin/bd.php
+++ admin/bd.php
@@ -5,7 +5,7 @@ $puerto="3307";
 $baseDatos="comedor_universitario";
 $usuario="root";
 $contrasenia="";
-
+$db=mysql_connect("$servidor","$puerto","$baseDatos","$usuario","$contrasenia");
 try{
 
     $conexion = new PDO("mysql:host=$servidor;port=$puerto;dbname=$baseDatos",$usuario,$contrasenia);
